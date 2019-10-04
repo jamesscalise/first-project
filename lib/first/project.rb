407aeb815d681298d7a24d494c57345564e6eec1
James Scalise <jamescscalise@gmail.com>
2019-10-04 02:10:32 +0000

    first commit

---
+++ lib/first/project.rb
@@ -0,0 +1,8 @@
+require "first/project/version"
+
+module First
+  module Project
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/first/project/version.rb
@@ -0,0 +1,5 @@
+module First
+  module Project
+    VERSION = "0.1.0"
+  end
+end
